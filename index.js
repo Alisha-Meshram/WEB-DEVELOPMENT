fb8fb53b33af1a57befaab7110b39c3fa1e43f36
Alisha-Meshram <aalishameshram15@gmail.com>
2025-06-07 12:38:20 +0530

    add calculator

--- index.js
+++ index.js
@@ -24,10 +24,25 @@ console.log("hello world")
     
 // }
 
-function ashu(name) {
-    console.log("i am " + name+ " a teacher");
-    console.log("i am " + name+  " a  doctor");
-    console.log("i am " + name+ " a nurse");
-}
-ashu("all type of ");
+// function ashu(name) {
+//     console.log("i am " + name+ " a teacher");
+//     console.log("i am " + name+  " a  doctor");
+//     console.log("i am " + name+ " a nurse");
+// }
+// ashu("all type of ");
 
+let a = "alisha";
+
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+let a_name="alisha";
+let friend="sanju";
+
+console.log("my name is " + a_name + " my friend name is " + friend);
+console.log(`my name is ${a_name} and my friend name is ${friend}`);
