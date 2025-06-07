892fe28e68c2ac4f055953e3c844a72a26aa6fb2
Alisha-Meshram <aalishameshram15@gmail.com>
2025-06-07 10:58:01 +0530

    add new js file

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("hello world")
+
+// let a={
+//     phone: "39087",
+//     emil:"alidg@123"
+// }
+
+// console.log(typeof a);
+// let b=-1;
+// if(b>10) {
+//     console.log("this is eligible for voting");
+// }
+// else if(b<18){
+//     console.log("this is not eligible for voting");
+// }
+// else{
+//     console.log("this is not or yes eligible for voting");
+// }
+
+
+// let b=2;
+// for (let i = 0; i < 100; i++) {
+//     console.log(b*i);
+    
+// }
+
+function ashu(name) {
+    console.log("i am " + name+ " a teacher");
+    console.log("i am " + name+  " a  doctor");
+    console.log("i am " + name+ " a nurse");
+}
+ashu("all type of ");
+
